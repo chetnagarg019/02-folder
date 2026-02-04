d2e39b9a19e97435d7c09b5ee64bc402c8d2877f
chetna <chetnagarg019@gmail.com>
2026-02-04 19:48:23 +0530

    third commit

--- api-call/src/App.jsx
+++ api-call/src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
 
-  function handleClick(){
-   console.log("Data aa gya");
+  const [data,setData] = useState([]);
+
+  async function handleClick(){
+   const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+   const data = await response.json();
+   setData(data);
+   
+
    
   }
 
@@ -11,6 +17,12 @@ const App = () => {
   return (
    <>
    <button onClick={handleClick} className='bg-gray-300 px-2 py-1.5 rounded-2xl mt-4'>Start Calling api!</button>
+   <div>
+    {data.map(function(ele,idx){
+      return <h2>hello ,{ele.id}, {idx}, {ele.url}  </h2>
+    })}
+   </div>
+    
    </>
   )
 }
